1591a43c93edabd0dc3fa70bc7d9d3b8ab1cfddb
LOKESH REDDY <lokeshreddygandu06@gmail.com>
2026-09-17 19:30:16 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://kisanflow-backend-5b7l.onrender.com",
 });
 
 export const getCentres = () => API.get("/centres");
